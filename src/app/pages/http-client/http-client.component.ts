7e15883116623575b7258133fd3e435daeb5111a
Ilya Chupin <ilya.chupin@itwis.org>
2021-12-28 18:09:03 +0700

    complete lesson 66

--- src/app/pages/http-client/http-client.component.ts
+++ src/app/pages/http-client/http-client.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Todo} from "../../interfaces/todo.interface";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-http-client',
@@ -9,9 +10,15 @@ import {Todo} from "../../interfaces/todo.interface";
 })
 export class HttpClientComponent implements OnInit {
 
+  form: FormGroup;
   todos: Todo[] = [];
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private formBuilder: FormBuilder) {
+
+    this.form = this.formBuilder.group({
+      title: [null, Validators.required],
+      completed: [false]
+    })
 
   }
 
@@ -21,4 +28,18 @@ export class HttpClientComponent implements OnInit {
     })
   }
 
+  onSubmit() {
+
+    if (this.form.invalid) {
+      const controls = this.form.controls;
+      Object.keys(controls)
+        .forEach(controlName => controls[controlName].markAsTouched());
+      return;
+    }
+
+    this.http.post<Todo>('https://jsonplaceholder.typicode.com/todos/', this.form.value).subscribe(todo => {
+      this.todos.unshift(todo);
+      this.form.reset();
+    })
+  }
 }
